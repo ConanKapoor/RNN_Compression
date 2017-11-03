6ea2a86144bc2859d0e5c098f36df0036102f81f
absingh31 <absinghemail@gmail.com>
2017-11-03 10:54:11 +0530

    datasets

---
+++ dataset/input_data.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+
+# Data dimensions
+#
+num_train = 100000
+num_test = 10000
+max_length = 100
+num_features = 1
+
+_path = '/'.join(__file__.split('/')[:-1])
+os.makedirs(_path+'/bin')
+
+# Training data
+#
+if not os.path.isfile(_path+'/bin/xs_train.npy'):
+	xs_train = np.random.rand(num_train, max_length, num_features)
+	np.save(_path+'/bin/xs_train.npy', xs_train)
+else:
+	xs_train = np.load(_path+'/bin/xs_train.npy')
+if not os.path.isfile(_path+'/bin/ls_train.npy'):
+	ls_train = np.random.randint(0, max_length, num_train)
+	np.save(_path+'/bin/ls_train.npy', ls_train)
+else:
+	ls_train = np.load(_path+'/bin/ls_train.npy')
+ys_train = np.round(ls_train.astype(float)/max_length)
+
+
+# Test data
+#
+if not os.path.isfile(_path+'/bin/xs_test.npy'):
+	xs_test = np.random.rand(num_test, max_length, num_features)
+	np.save(_path+'/bin/xs_test.npy', xs_test)
+else:
+	xs_test = np.load(_path+'/bin/xs_test.npy')    
+if not os.path.isfile(_path+'/bin/ls_test.npy'):
+	ls_test = np.random.randint(0, max_length, num_test)
+	np.save(_path+'/bin/ls_test.npy', ls_test)
+else:
+	ls_test = np.load(_path+'/bin/ls_test.npy')
+ys_test = np.round(ls_test.astype(float)/max_length)
+
